e90b3e011dd913a86366afaacea6015c4d3a6a71
Tyler Robbins <tyler@familyrobbins.com>
2014-10-16 17:27:00 -0700

    Begun writing the main Robot class.

---
+++ Robot.cpp
@@ -0,0 +1,19 @@
+#include "WPILib.h"
+
+class Robot : public SimpleRobot
+{
+	public:
+		Robot(void){
+			//Initialization Code
+		}
+
+		void Autonomous(void){
+			//Autonomous Code
+		}
+
+		void OperatorControl(void){
+			//Teleop Code
+		}
+};
+
+START_ROBOT_CLASS(Robot);
