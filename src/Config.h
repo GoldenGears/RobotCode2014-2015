9d48b4e290dcc1f920aefdd5328eef8130219931
Tyler Robbins <tyler@familyrobbins.com>
2014-11-07 09:28:45 -0800

    Added a config file to store all configuration settings.

---
+++ src/Config.h
@@ -0,0 +1,26 @@
+//***********************
+// Golden Gears Robotics
+// 11/6/14
+// Config H
+//***********************
+
+// 11-6-14 - Tyler Robbins - Added a config file to store all configuration settings.
+
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+namespace CONFIG{
+	namespace HARDWARE{
+		// All hardware ports go here
+		//    i.e: motors
+	}
+
+	namespace INPUT{
+		// All input ports go here
+		//    i.e: joystick buttons
+	}
+
+	// Any additional namespaces and functions should go here.
+}
+
+#endif
