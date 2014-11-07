9ba29a753800f018ad5e4469c9c687d1be2ed246
Tyler Robbins <tyler@familyrobbins.com>
2014-11-07 09:27:37 -0800

    Added a header. Added more imports. Added define statements. Defined more methods and variables.

--- src/Robot.h
+++ src/Robot.h
@@ -1,11 +1,39 @@
+//*******************
+// Golden Gears Robotics
+// 11/6/14
+// Robot H
+//*******************
+
+// 11-6-14 - Tyler Robbins - Added a header. Added more imports. Added define statements. Defined more methods and variables.
+
 #include "WPILib.h"
+#include "RobotBase.h"
+#include "IterativeRobot.h"
+
+#ifndef _ROBOT_H_
+#define _ROBOT_H_
 
 class Robot : public IterativeRobot {
+	Joystick stick;
+	
 public:
-	Robot(void);
 	~Robot();
 
-	void Autonomous(void);
+	static Robot& instance(){ return (Robot&) RobotBase::getInstance(); };
 
-	void OperatorControl(void);
-}
+	void RobotInit(void) override;
+
+	void AutonomousPeriodic(void) override;
+
+	// void OperatorControl(void);
+
+	void TeleopInit(void) override;
+	void TeleopPeriodic(void) override;
+
+private:
+	friend RobotBase* FRC_userClassFactory();
+
+	Robot();
+};
+
+#endif
