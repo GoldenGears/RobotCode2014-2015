9d4cca91d9bd12509085cffc12f19675c5986db7
Tyler Robbins <tyler@familyrobbins.com>
2014-11-15 13:58:12 -0800

    Added a blank config.cpp file that will define any functions we declare in Config.h

---
+++ src/Config.cpp
@@ -0,0 +1,16 @@
+//***********************
+// Golden Gears Robotics
+// 11/6/14
+// Config CPP
+//***********************
+
+// 11-12-14 - Tyler Robbins - Added a blank config.cpp file that will define any functions we declare in Config.h
+
+#include "Config.h"
+
+
+// Define all functions here.
+
+
+
+
