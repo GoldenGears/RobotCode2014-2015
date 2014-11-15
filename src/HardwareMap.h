3b3686937bf1d4d87bf7b586bba1b5f26d07d178
Tyler Robbins <tyler@familyrobbins.com>
2014-11-15 14:00:25 -0800

    Created a file to map all hardware to software objects.

---
+++ src/HardwareMap.h
@@ -0,0 +1,21 @@
+//************************
+// Golden Gears Robotics
+// 11/13/14
+// Hardware Map H
+//************************
+
+// 11-13-14 - Tyler Robbins - Created a file to map all hardware to software objects.
+
+#ifndef _HARDWARE_MAP_H_
+#define _HARDWARE_MAP_H_
+
+class HardwareMap
+{
+public:
+	HardwareMap();
+	~HardwareMap();
+
+	void init();
+};
+
+#endif
