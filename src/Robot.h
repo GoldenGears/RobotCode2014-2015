f9bc8525769a420d9b8c5650c3ca935f968888de
Tyler Robbins <tyler@familyrobbins.com>
2014-11-23 11:37:58 -0800

    Added some more include statements. Removed an unused variable and some commented out code. Added some more class members.

--- src/Robot.h
+++ src/Robot.h
@@ -6,10 +6,16 @@
 
 // 11-6-14 - Tyler Robbins - Added a header. Added more imports. Added define statements. Defined more methods and variables.
 // 11-13-14 - Tyler Robbins - Added an enumeration. Defined a new function.
+// 11-22-14 - Tyler Robbins - Added some more include statements. Removed an unused variable and some commented out code. Added some more class members.
 
 #include "WPILib.h"
 #include "RobotBase.h"
 #include "IterativeRobot.h"
+
+#include "HardwareMap.h"
+#include "OI.h"
+
+#include "Subsystems/MotorSubsystem.h"
 
 #ifndef _ROBOT_H_
 #define _ROBOT_H_
@@ -19,7 +25,6 @@ enum class Direction {UP,DOWN};
 const char* str_direction(Direction dir);
 
 class Robot : public IterativeRobot {
-	Joystick stick;
 	
 public:
 	~Robot();
@@ -30,12 +35,16 @@ public:
 
 	void AutonomousPeriodic(void) override;
 
-	// void OperatorControl(void);
-
 	void TeleopInit(void) override;
 	void TeleopPeriodic(void) override;
 
 private:
+	HardwareMap* m_hw_map;
+
+	OI* m_oi;
+
+	Drive* m_drive;
+
 	friend RobotBase* FRC_userClassFactory();
 
 	Robot();
