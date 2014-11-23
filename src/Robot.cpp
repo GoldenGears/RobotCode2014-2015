c95ccd9ed9ad49a7b6e6a22e823ff2d54e7a45d3
Tyler Robbins <tyler@familyrobbins.com>
2014-11-23 11:40:27 -0800

    Added some stuff to the destructor. Added some stuff to the member initialization list in the constructor.

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -6,6 +6,7 @@
 
 //11-6-14 - Tyler Robbins - Added header. Added some new empty methods. Added more comments. Removed old commented out code.
 //11-14-14 - Tyler Robbins - Added some more include statements. Defined a new function. Removed unused attribute.
+//11-23-14 - Tyler Robbins - Added some stuff to the destructor. Added some stuff to the member initialization list in the constructor.
 
 #include "WPILib.h"
 
@@ -17,12 +18,19 @@ const char* str_direction(Direction dir){
 }
 
 Robot::Robot(void)
-	:
+	: m_hw_map(nullptr),
+	m_oi(nullptr),
+	m_drive(nullptr){
 
-	/*Initialize all necessary variables*/
-	{ }
+}
 
-Robot::~Robot(){ }
+Robot::~Robot(){ 
+	// Needed to clear memory in case the Robot class gets deleted
+	// Delete in reverse construction order
+	delete m_drive;
+	delete m_oi;
+	delete m_hw_map;
+}
 
 Robot::Autonomous(void){
 	/*All Autonomous code should go here.*/
