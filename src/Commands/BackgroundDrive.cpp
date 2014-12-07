224fd73fd22ff15738d8f689d100bb8b8031fd4c
Tyler Robbins <tyler@familyrobbins.com>
2014-12-07 00:06:57 -0800

    Added new include and forward class declaration. Constructor now only accepts one parameter. Specified the type of methods. joy is now gotten directly from Robot::oi().

--- src/Commands/BackgroundDrive.cpp
+++ src/Commands/BackgroundDrive.cpp
@@ -5,31 +5,40 @@
 //*******************
 
 // 11-23-14 - Tyler Robbins - Created a file that declares the BackgroundDrive class and its methods.
+// 12-6-14 - Tyler Robbins - Added new include and forward class declaration. Constructor now only accepts one parameter. Specified the type of methods. joy is now gotten directly from Robot::oi().
 
 #include "BackgroundDrive.h"
 #include "Joystick.h"
 
 #include "../Subsystems/Drive.h"
+#include "../Robot.h"
 
-BackgroundDrive::BackgroundDrive(Drive *drive, Joystick *joy)
-	: Command("BackgroundDrive"),
-	m_drive(drive),
-	m_joy(joy) {
+class Drive;
+
+// BackgroundDrive::BackgroundDrive(Drive* drive, Joystick *joy)
+// BackgroundDrive::BackgroundDrive()
+BackgroundDrive::BackgroundDrive(Drive* drive)
+	: Command("BackgroundDrive")
+	,m_drive(drive)
+	// ,m_joy(joy) {
+	{
 		Requires(drive);
+		// Requires(Robot::drive());
 }
 
 BackgroundDrive::~BackgroundDrive(){}
 
-BackgroundDrive::Initialize(){}
+void BackgroundDrive::Initialize(){}
 
-BackgroundDrive::Execute(){
+void BackgroundDrive::Execute(){
+	Joystick &joy = Robot::oi()->joy;
 	float x,y,turn,throttle;
 
-	x = m_joy->GetX();
-	y = m_joy->GetY();
+	x = joy.GetX();
+	y = joy.GetY();
 
-	turn = m_joy->GetThrottle(); // For some wierd reason, these are swapped according to last year's code.
-	throttle = m_joy->GetTwist(); // We should test this just to be sure though.
+	turn = joy.GetThrottle(); // For some wierd reason, these are swapped according to last year's code.
+	throttle = joy.GetTwist(); // We should test this just to be sure though.
 
 	// Add 1 to shift the values and then divide by 2 to scale down
 	throttle = (-throttle + 1.0)/2.0; // Note: Not sure why this is necessary. Should contact Philip or Matt and ask them.
@@ -47,12 +56,13 @@ BackgroundDrive::Execute(){
 	turn *= throttle;
 
 	m_drive->drive(x,y,turn);
+	// Robot::drive()
 }
 
-BackgroundDrive::isFinished(){
+bool BackgroundDrive::isFinished(){
 	return false;
 }
 
-BackgroundDrive::End(){}
+void BackgroundDrive::End(){}
 
-BackgroundDrive::Interrupted(){}
+void BackgroundDrive::Interrupted(){}
