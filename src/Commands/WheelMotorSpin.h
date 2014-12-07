30a85812f968cb9ba974a6a567188c7cce60ee50
Tyler Robbins <tyler@familyrobbins.com>
2014-12-07 00:08:27 -0800

    Quick Edit: Changed first-commit-log file description.

--- src/Commands/WheelMotorSpin.h
+++ src/Commands/WheelMotorSpin.h
@@ -4,7 +4,7 @@
 // Wheel Motor Spin H
 //*******************
 
-// 11-22-14 - Tyler Robbins - Created a file that defines the WheelMotorSpin class which will be used as a command for turning the wheels.
+// 11-22-14 - Tyler Robbins - Created a file that defines the WheelMotorSpin class which will be used as a command for turning the arm wheels.
 
 #ifndef _COMMANDS_WHEEL_MOTOR_SPIN_H
 #define _COMMANDS_WHEEL_MOTOR_SPIN_H
