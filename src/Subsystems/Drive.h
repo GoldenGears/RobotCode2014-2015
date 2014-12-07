2bf8e15c2f712acec144b7df454f64f0b293f5bb
Tyler Robbins <tyler@familyrobbins.com>
2014-12-06 23:47:37 -0800

    Added new forward declaration. Removed parameters from constructor. Made *m_def_command a constant.

--- src/Subsystems/Drive.h
+++ src/Subsystems/Drive.h
@@ -5,12 +5,14 @@
 //*******************
 
 // 11-23-14 - Tyler Robbins - Created a file that defines the Drive class and its methods.
+// 12-6-14 - Tyler Robbins - Added new forward declaration. Removed parameters from constructor. Made *m_def_command a constant.
 
 #ifndef _SUBSYSTEMS_DRIVE_H
 #define _SUBSYSTEMS_DRIVE_H 
 
 class Joystick;
 class Talon;
+class BackgroundDrive;
 
 #include "Commands/Subsystem.h"
 #include "RobotDrive.h"
@@ -20,8 +22,10 @@ class Talon;
 class Drive : public Subsystem
 {
 public:
-	Drive(Talon *front_l, Talon *front_r,
-		Talon back_l, Talon back_r, Joystick *joy);
+	// Drive(Talon *left_1, Talon *left_2, Talon *left_3, 
+	// Talon *right_1, Talon *right_2, Talon *right_3,
+	// Joystick *joy);
+	Drive();
 	~Drive() override;
 
 	void InitDefaultCommand() override;
@@ -31,7 +35,7 @@ public:
 private:
 	RobotDrive robot_drive;
 
-	BackgroundDrive *m_def_command;
+	const BackgroundDrive *m_def_command;
 };
 
 #endif
