a0782c373e647eb066c9b7a1001679ed3197d155
Tyler Robbins <tyler@familyrobbins.com>
2014-12-06 23:32:19 -0800

    Added a forward declaration for the Drive class. Added some new methods and removed the void parameter from all other methods. Changed constructor to be private.

--- src/Robot.h
+++ src/Robot.h
@@ -7,6 +7,7 @@
 // 11-6-14 - Tyler Robbins - Added a header. Added more imports. Added define statements. Defined more methods and variables.
 // 11-13-14 - Tyler Robbins - Added an enumeration. Defined a new function.
 // 11-23-14 - Tyler Robbins - Added some more include statements. Removed an unused variable and some commented out code. Added some more class members.
+// 12-6-14 - Tyler Robbins - Added a forward declaration for the Drive class. Added some new methods and removed the void parameter from all other methods. Changed constructor to be private.
 
 #include "WPILib.h"
 #include "RobotBase.h"
@@ -20,6 +21,8 @@
 #ifndef _ROBOT_H_
 #define _ROBOT_H_
 
+class Drive;
+
 enum class Direction {UP,DOWN};
 
 const char* str_direction(Direction dir);
@@ -31,14 +34,23 @@ public:
 
 	static Robot& instance(){ return (Robot&) RobotBase::getInstance(); };
 
-	void RobotInit(void) override;
+	void RobotInit() override;
 
-	void AutonomousPeriodic(void) override;
+	void AutonomousPeriodic() override;
 
-	void TeleopInit(void) override;
-	void TeleopPeriodic(void) override;
+	void TeleopInit() override;
+	void TeleopPeriodic() override;
+
+	void DisabledInit() override;
+	void DisabledPeriodic() override;
+
+	static HardwareMap* hw_map() { return instance().m_hw_map; };
+	static Drive* drive() { return instance().m_drive; };
+	static OI* oi() { return instance().m_oi; };
 
 private:
+	Robot();
+
 	HardwareMap* m_hw_map;
 
 	OI* m_oi;
@@ -46,8 +58,6 @@ private:
 	Drive* m_drive;
 
 	friend RobotBase* FRC_userClassFactory();
-
-	Robot();
 };
 
 #endif
