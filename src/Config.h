075b7ceb577f0e92572c5f3be74c201aa061c373
Tyler Robbins <tyler@familyrobbins.com>
2014-12-06 23:39:18 -0800

    Changed motor ports to allow for 6 motors (three on the left and three on the right). Added port numbers for the motors and joystick.

--- src/Config.h
+++ src/Config.h
@@ -6,6 +6,7 @@
 
 // 11-6-14 - Tyler Robbins - Added a config file to store all configuration settings.
 // 11-13-14 - Tyler Robbins - Added input constants.
+// 12-6-14 - Tyler Robbins - Changed motor ports to allow for 6 motors (three on the left and three on the right). Added port numbers for the motors and joystick.
 
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
@@ -15,18 +16,22 @@ namespace CONFIG{
 		// All hardware ports go here
 		//    i.e: motors
 
-		const int motorL;
-		const int motorR;
+		const int motorL1 = 1;
+		const int motorL2 = 2;
+		const int motorL3 = 3;
+		const int motorR1 = 4;
+		const int motorR2 = 5;
+		const int motorR3 = 6;
 	}
 
 	namespace INPUT{
 		// All input ports go here
 		//    i.e: joystick buttons
 
-		const int joystick1;
+		const int joystick1 = 1;
 
-		const int bForward;
-		const int bBackward;
+		const int bForward = 0; // These aren't used for anything currently, but they must be initialized anyway.
+		const int bBackward = 0;
 	}
 
 	// Any additional namespaces and functions should go here.
