311811cfaa273954a26110070d49cade53eb2fd2
Tyler Robbins <tyler@familyrobbins.com>
2014-12-07 00:01:29 -0800

    Added two new includes and a forward class declaration. isFinished no longer overrides. Constructor is now explicit. Removed empty curly brakets {}. Commented out m_joy.

--- src/Commands/BackgroundDrive.h
+++ src/Commands/BackgroundDrive.h
@@ -7,26 +7,33 @@
 //*******************
 
 // 11-23-14 - Tyler Robbins - Created a file that defines the BackgroundDrive class and its methods.
+// 12-6-14 - Tyler Robbins - Added two new includes and a forward class declaration. isFinished no longer overrides. Constructor is now explicit. Removed empty curly brakets {}. Commented out m_joy.
 
 #ifndef _COMMANDS_BACKGROUND_DRIVE_H_
 #define _COMMANDS_BACKGROUND_DRIVE_H_ 
 
+#include "Joystick.h"
 #include "Commands/Command.h"
 
+#include "../Subsystems/Drive.h"
+
+class Drive;
+
 class BackgroundDrive : public Command{
-public:
-	BackgroundDrive(Drive *drive, Joystick *joy);
-	~BackgroundDrive() override;
+	public:
+		// BackgroundDrive(Drive *drive, Joystick *joy);
+		explicit BackgroundDrive(Drive *drive);
+		~BackgroundDrive() override;
 
-	void Initialize(){} override;
-	void Execute(){} override;
-	void isFinished(){} override;
-	void End(){} override;
-	void Interrupted(){} override;
+		void Initialize() override;
+		void Execute() override;
+		bool isFinished();
+		void End() override;
+		void Interrupted() override;
 
-private:
-	Drive *m_drive;
-	Joystick *m_joy;
+	private:
+		Drive *m_drive;
+		// Joystick *m_joy;
 };
 
 #endif
