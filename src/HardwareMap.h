1fdf7f3d896190a8ed73757931ef20dc935cfaa9
Tyler Robbins <tyler@familyrobbins.com>
2014-12-06 23:43:25 -0800

    Added an include. Added 6 Talon objects.

--- src/HardwareMap.h
+++ src/HardwareMap.h
@@ -5,9 +5,12 @@
 //************************
 
 // 11-13-14 - Tyler Robbins - Created a file to map all hardware to software objects.
+// 12-6-14 - Tyler Robbins - Added an include. Added 6 Talon objects.
 
 #ifndef _HARDWARE_MAP_H_
 #define _HARDWARE_MAP_H_
+
+#include "Talon.h"
 
 class HardwareMap
 {
@@ -16,6 +19,17 @@ public:
 	~HardwareMap();
 
 	void init();
+
+	// All Hardware variables go here
+
+	// Drive Talons
+	Talon left_1;
+	Talon left_2;
+	Talon left_3;
+
+	Talon right_1;
+	Talon right_2;
+	Talon right_3;
 };
 
 #endif
