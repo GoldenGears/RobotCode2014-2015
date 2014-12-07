719e8dc56d78c39afc9277a03e0fa6f84c056b69
Tyler Robbins <tyler@familyrobbins.com>
2014-12-07 00:12:22 -0800

    Removed Requires statement in constructor. Commented out code for the Execute, End, and Interrupted methods. Fixed capitalization of the IsFinished method.

--- src/Commands/WheelMotorSpin.cpp
+++ src/Commands/WheelMotorSpin.cpp
@@ -5,6 +5,7 @@
 //*******************
 
 // 11-22-14 - Tyler Robbins - Created a file that declares the WheelMotorSpin class and its methods.
+// 12-6-14 - Tyler Robbins - Removed Requires statement in constructor. Commented out code for the Execute, End, and Interrupted methods. Fixed capitalization of the IsFinished method.
 
 #include "WheelMotorSpin.h"
 
@@ -16,7 +17,7 @@
 WheelMotorSpin::WheelMotorSpin(Direction dir)
 	: Command("WheelMotorSpin"),
 	m_direct(dir) {
-	Requires(Robot::arm_wheels());
+	// Requires(Robot::arm_wheels());
 }
 
 WheelMotorSpin::~WheelMotorSpin() {}
@@ -24,17 +25,17 @@ WheelMotorSpin::~WheelMotorSpin() {}
 void WheelMotorSpin::Initialize() {}
 
 void WheelMotorSpin::Execute() {
-	Robot::arm_wheels()->move(m_direct);
+	// Robot::arm_wheels()->move(m_direct);
 }
 
-bool WheelMotorSpin::isFinished() {
+bool WheelMotorSpin::IsFinished() {
 	return false;
 }
 
 void WheelMotorSpin::End() {
-	Robot::arm_wheels()->stop();
+	// Robot::arm_wheels()->stop();
 }
 
 void WheelMotorSpin::Interrupted() {
-	Robot::arm_wheels()->stop();
+	// Robot::arm_wheels()->stop();
 }
