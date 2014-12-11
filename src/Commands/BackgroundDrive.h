4db26230990103eb844b960f1ac95c44c13f8c71
Tyler Robbins <tyler@familyrobbins.com>
2014-12-11 09:30:15 -0800

    Fixed method overrides. Fixed constructor declaration.

--- src/Commands/BackgroundDrive.h
+++ src/Commands/BackgroundDrive.h
@@ -6,8 +6,9 @@
 // Checks for joystick input first, then drives the Robot.
 //*******************
 
-// 11-23-14 - Tyler Robbins - Created a file that defines the BackgroundDrive class and its methods.
+// 11-23-14 - Tyler Robbins - Created a file that declares the BackgroundDrive class and its methods.
 // 12-6-14 - Tyler Robbins - Added two new includes and a forward class declaration. isFinished no longer overrides. Constructor is now explicit. Removed empty curly brakets {}. Commented out m_joy.
+// 12-11-14 - Tyler Robbins - Fixed method overrides. Fixed constructor declaration. 
 
 #ifndef _COMMANDS_BACKGROUND_DRIVE_H_
 #define _COMMANDS_BACKGROUND_DRIVE_H_ 
@@ -21,19 +22,18 @@ class Drive;
 
 class BackgroundDrive : public Command{
 	public:
-		// BackgroundDrive(Drive *drive, Joystick *joy);
-		explicit BackgroundDrive(Drive *drive);
+		BackgroundDrive(Drive *drive, Joystick *joy);
 		~BackgroundDrive() override;
 
 		void Initialize() override;
 		void Execute() override;
-		bool isFinished();
+		bool IsFinished() override;
 		void End() override;
 		void Interrupted() override;
 
 	private:
 		Drive *m_drive;
-		// Joystick *m_joy;
+		Joystick *m_joy;
 };
 
 #endif
