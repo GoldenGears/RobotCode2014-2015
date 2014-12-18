1cc72a4c00ab9636e7702cf2497b96708fe1c303
Tyler Robbins <tyler@familyrobbins.com>
2014-12-18 15:43:24 -0800

    Constructor now takes second left and right Talons. Added new robotDrive instantiations. Replaced robot_drive with driveFront/Middle/Back.

--- src/Subsystems/Drive.cpp
+++ src/Subsystems/Drive.cpp
@@ -7,6 +7,7 @@
 // 11-23-14 - Tyler Robbins - Created a file that declares the Drive class and its methods.
 // 12-6-14 - Tyler Robbins - Removed includes for Joystick and Talon. Changed robot_drive initialization. Specified InitDefaultCommand and drive as void.
 // 12-11-14 - Tyler Robbins - Fixed constructor definition. Changed SetDefaultCommand() call by passing in a pointer rather than a dereferenced pointer.
+// 12-18-14 - Tyler Robbins - Constructor now takes second left and right Talons. Added new robotDrive instantiations. Replaced robot_drive with driveFront/Middle/Back.
 
 #include "Drive.h"
 
@@ -17,19 +18,28 @@
 #include "../Commands/BackgroundDrive.h"
 #include "../Robot.h"
 
-Drive::Drive(Talon *left_1, Talon *left_3, 
-	Talon *right_1, Talon *right_3,
+// Drive::Drive(Talon *left_1, Talon *left_3, 
+// 	Talon *right_1, Talon *right_3,
+// 	Joystick *joy)
+Drive::Drive(Talon *left_1, Talon *left_2, Talon *left_3, 
+	Talon *right_1, Talon *right_2, Talon *right_3,
 	Joystick *joy)
 	: Subsystem("Drive"),
-	robot_drive(left_1,
-				left_3,
-				right_1,
-				right_3)
+	// robot_drive(left_1,
+	// 			left_3,
+	// 			right_1,
+	// 			right_3)
+	driveFront(left_1,right_1),
+	driveMiddle(left_2,right_2),
+	driveBack(left_3,right_3)
 	
 	,m_def_command(new BackgroundDrive(this,joy))
 	{
 
-	robot_drive.SetExpiration(0.5);
+	// robot_drive.SetExpiration(0.5);
+	driveFront.SetExpiration(0.5);
+	driveMiddle.SetExpiration(0.5);
+	driveBack.SetExpiration(0.5);
 
 	// In case any of the motors are inverted, we can correct them by un-commenting the corresponding motor
 
@@ -50,5 +60,8 @@ void Drive::InitDefaultCommand(){
 void Drive::drive(float x, float y, float turn){
 	// y is used so that when we push the joystick forwards, the robot moves forwards.
 	// We should probably add code to use different driving methods depending on driver preference, but this should be good enough for now.
-	robot_drive.ArcadeDrive(y,turn); 
+	// robot_drive.ArcadeDrive(y,turn); 
+	driveFront.ArcadeDrive(y,turn);
+	driveMiddle.ArcadeDrive(y,turn);
+	driveBack.ArcadeDrive(y,turn);
 }
